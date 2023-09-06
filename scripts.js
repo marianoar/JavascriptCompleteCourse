beeb745b6def397fecb96006be2bf506b538eab1
Mariano Luis Arias <mariano.arias@neoris.com>
2023-09-06 14:54:22 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,36 @@
+'use strict';
+console.log('test');
+// Problem I
+// we have a serie of data temperature and we need to get max, min and amplitude
+const temperatures = [3, -2, -6, -1, 9, 13, 'error', 17, 15, 14, 9, 5];
+
+//how to get a maximun value from an array
+const calcTemAmplitude = function (anArray) {
+  let max = anArray[0];
+  let min = anArray[0];
+
+  for (let i = 0; i < anArray.length; i++) {
+    if (typeof anArray[i] !== 'number') continue;
+    if (anArray[i] > max) {
+      max = anArray[i];
+    }
+    if (anArray[i] < min) min = anArray[i];
+  }
+  console.log('max: ' + max);
+  console.log('min: ' + min);
+  return max - min;
+};
+
+const amplitude = calcTemAmplitude(temperatures);
+console.log('amplitude: ' + amplitude);
+
+//Problem II
+// Same problem but with two arrays
+// solution proposed is merge arrays into one
+
+const secondTemperatures = [6, 13, 23, -4, -4, 9, 0, 19];
+
+const array3 = temperatures.concat(secondTemperatures);
+
+const amplitude2 = calcTemAmplitude(array3);
+console.log('second amplitude: ' + amplitude2);
